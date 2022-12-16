b8f844585faad376aa78b051b8c980e77b9d038a
Matheus Fortes <matheusoda88@gmail.com>
2022-12-15 22:46:19 -0300

    1

--- FinalDesafio/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FinalDesafio.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
