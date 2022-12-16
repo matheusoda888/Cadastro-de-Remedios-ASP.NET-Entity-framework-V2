ec35c2d5a970bc5aae8cd9c7a7507b12b88f6f46
Matheus Fortes <58231046+matheusoda888@users.noreply.github.com>
2022-12-15 23:26:59 -0300

    Update QueryDesafioFinal2.sql

--- QueryDesafioFinal2.sql
+++ QueryDesafioFinal2.sql
@@ -5,7 +5,6 @@ create login AtosEntity9 with password='Atos_Entity_9';
 create user AtosEntity9 from login AtosEntity9;
 
 
-
 exec sp_addrolemember 'DB_DATAREADER', 'AtosEntity9';
 exec sp_addrolemember 'DB_DATAWRITER', 'AtosEntity9';
 
@@ -13,21 +12,17 @@ exec sp_addrolemember 'DB_DATAWRITER', 'AtosEntity9';
 create table Remedios(
 	id integer primary key identity,
 	nome varchar(30) not null,
-	
 )
-
 create table Pacientes(
 	id integer primary key identity,
 	nome varchar(30) not null,
 )
-
 create table Horarios(
 	id integer primary key identity,
-	nomeRemedio varchar(30) not null,
-	nomePaciente varchar(30) not null,
+	idRemedio integer not null,
+	idPaciente integer not null,
 	tempo integer not null,
 	horario time not null,
-	foreign key (nomeRemedio) references Remedios(nome),
-	foreign key (nomePaciente) references Pacientes(nome)
+	foreign key (idRemedio) references Remedios(id),
+	foreign key (idPaciente) references Pacientes(id)
 )
-
